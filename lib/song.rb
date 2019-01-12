7eea27e29a5316492d8980790f391f7196237e52
Christian V <viray.christianj@gmail.com>
2019-01-12 01:31:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,16 +29,16 @@ class Song
   
   def self.genre_count
     genre_count = Hash.new(0)
-    @@genres.each do |g|
-      genre_count[g] += 1
+    @@genres.each do |genre|
+      genre_count[genre] += 1
   end
     genre_count
   end
   
    def self.artist_count
     artist_count = Hash.new(0)
-    @@artists.each do |a|
-      artists_count[a] += 1
+    @@artists.each do |artist|
+      artists_count[artist] += 1
     end
     artist_count
   end
